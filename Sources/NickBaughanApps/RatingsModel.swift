2f0c74fabfca73e2419e344fe11025dd843abb86
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2023-07-03 17:31:30 +0100

    New app icon. New tutorial design. Layout changes to settings. Default focus in the add lesson view. Fixed silent widget provider errors. Fixed widget layout to use Swift Charts.

--- Sources/NickBaughanApps/RatingsModel.swift
+++ Sources/NickBaughanApps/RatingsModel.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 import StoreKit
+import OSLog
 
-public struct RatingsModel {
+public struct RatingsModel: Sendable {
+    static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Ratings")
     static let lastReviewVersionKey = "lastReviewVersion"
     static let earliestReviewDate = "minimumReviewDate"
     static let appLaunchedPreviously = "appLaunchedPreviously"
@@ -16,11 +18,11 @@ public struct RatingsModel {
     static let significantActionCountKey = "significantActionCount"
     
     /// Set the earliest review date to 24 hours after today.
-    func markFirstLaunch() {
+    public static func markFirstLaunch() {
         let defaults = UserDefaults.standard
         if !defaults.bool(forKey: Self.appLaunchedPreviously) {
             defaults.set(true, forKey: Self.appLaunchedPreviously)
-            print("App launched first time")
+            logger.info("App launched first time")
             defaults.setValue(Date.now.addingTimeInterval(TimeInterval(86_400)).timeIntervalSinceReferenceDate, forKey: Self.earliestReviewDate)
         }
     }
@@ -32,31 +34,33 @@ public struct RatingsModel {
     /// - Not the current app version
     /// - Certain amount of time passed.  1 month?
     /// - Returns: Whether to present a review request.
-    func shouldPresentReview() -> Bool {
+    public static func shouldPresentReview() -> Bool {
         appendSignificantActionCount()
         let defaults = UserDefaults.standard
         let lastReviewVersion = try? JSONDecoder().decode(Version.self, from: defaults.data(forKey: Self.lastReviewVersionKey) ?? Data())
         let earliestReviewDate = Date(timeIntervalSinceReferenceDate: defaults.double(forKey: Self.earliestReviewDate))
-        if defaults.integer(forKey: Self.significantActionCountKey) >= 5 && lastReviewVersion ?? .version1 < .latest && earliestReviewDate > .now {
+        if defaults.integer(forKey: Self.significantActionCountKey) >= 5 && lastReviewVersion ?? .version1 < .latest && earliestReviewDate < .now {
             updateReview()
+            logger.info("Review should present")
             return true
         } else {
+            logger.info("Review should not present")
             return false
         }
     }
     
     /// Update UserDefaults with the details of the current review.
-    private func updateReview() {
-        print("Updating review")
+    private static func updateReview() {
+        logger.info("Updating review condition.")
         let defaults = UserDefaults.standard
         defaults.setValue(try? JSONEncoder().encode(Version.latest), forKey: Self.lastReviewVersionKey)
         defaults.setValue(Date.now.addingTimeInterval(TimeInterval(2_678_400)).timeIntervalSinceReferenceDate, forKey: Self.earliestReviewDate)
     }
     
-    private func appendSignificantActionCount() {
+    private static func appendSignificantActionCount() {
         var significantActionCount = UserDefaults.standard.integer(forKey: Self.significantActionCountKey)
         significantActionCount += 1
-        print("Updating action count to \(significantActionCount)")
+        logger.info("Updating action count to \(significantActionCount)")
         UserDefaults.standard.setValue(significantActionCount, forKey: Self.significantActionCountKey)
     }
 }
