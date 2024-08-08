cc1b5725d43dae98308255a3f7fe2a8371713d7e
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2024-08-08 19:48:24 +0100

    Added support email and developer website urls.

---
+++ Sources/NickBaughanApps/URL.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nick Baughan on 08/08/2024.
+//
+
+import Foundation
+
+public extension URL {
+    static let developerWebsite: Self = URL(string: "https://nickbaughan.com")!
+    static let supportEmail: Self = URL(string: "mailto:support@nickbaughan.com")!
+}
