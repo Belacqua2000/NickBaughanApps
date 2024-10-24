5d7f16c49520373c5b77575b05ebe10444976ad3
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2024-10-24 11:26:37 +0100

    Added an 'unseen versions' computed property.

--- Sources/NickBaughanApps/VersionManager.swift
+++ Sources/NickBaughanApps/VersionManager.swift
@@ -13,6 +13,13 @@ import Foundation
 public struct VersionManager: Sendable {
     public let allVersions: [Version]
     
+    public var unseenVersions: [Version] {
+        allVersions
+            .filter { $0 > lastSavedVersion ?? Version(majorNumber: 0, minorNumber: 0, thirdNumber: 0) }
+            .sorted()
+            .reversed()
+    }
+    
     public init(allVersions: [Version]) {
         self.allVersions = allVersions
     }
@@ -26,20 +33,16 @@ public struct VersionManager: Sendable {
     let lastSavedVersionKey = "WhatsNewVersionID"
     
     /// Get the last saved version from UserDefaults.
-    private(set) var lastSavedVersion: Version? {
+    public var lastSavedVersion: Version? {
         get {
             guard let lastSavedVersionID = UserDefaults.standard.string(forKey: lastSavedVersionKey) else { return nil }
             return allVersions.first { $0.id == lastSavedVersionID }
-        } set {
-            if let newValue {
-                UserDefaults.standard.set(newValue.id, forKey: lastSavedVersionKey)
-            }
         }
     }
     
     /// This will save the current version as the most up-to-date version.
-    public mutating func setCurrentVersionAsLastSaved() {
-        lastSavedVersion = currentVersion
+    public func setCurrentVersionAsLastSaved() {
+        UserDefaults.standard.set(currentVersion?.id, forKey: lastSavedVersionKey)
     }
     
 }
