b7265c9b6ef3ad7e1f8694b909c9561d64142d70
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2024-11-13 20:09:24 +0000

    Added Selectable modifier.

---
+++ Sources/NickBaughanApps/UI/Selectable.swift
@@ -0,0 +1,34 @@
+//
+//  Selectable.swift
+//  NickBaughanApps
+//
+//  Created by Nick Baughan on 13/11/2024.
+//
+
+import SwiftUI
+
+public struct Selectable: ViewModifier {
+    
+    public func body(content: Content) -> some View {
+        #if os(tvOS)
+        Button {} label: {
+            content
+        }
+        .foregroundStyle(.primary)
+        .accessibilityRemoveTraits(.isButton)
+        #else
+        content
+        #endif
+    }
+}
+
+extension View {
+    func selectable() -> some View {
+        modifier(Selectable())
+    }
+}
+
+#Preview {
+    Text("Hello, World!")
+        .selectable()
+}
