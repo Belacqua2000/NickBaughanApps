e8e6da42f8966e9a06f442a190b4ca61f89f54ca
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2024-11-17 17:44:17 +0000

    Support for platforms on Version.

--- Sources/NickBaughanApps/Version.swift
+++ Sources/NickBaughanApps/Version.swift
@@ -60,13 +60,51 @@ extension Version: Comparable {
     }
 }
 
+public struct Platforms: OptionSet, Sendable, Hashable {
+    public var rawValue: Int
+    
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    public static let macOS = Platforms(rawValue: 1 << 0)
+    public static let iOS = Platforms(rawValue: 1 << 1)
+    public static let tvOS = Platforms(rawValue: 1 << 2)
+    public static let watchOS = Platforms(rawValue: 1 << 3)
+    public static let visionOS = Platforms(rawValue: 1 << 4)
+    
+    /// All of the current Apple platforms.
+    public static let all: Platforms = [.macOS, .iOS, .tvOS, .watchOS, .visionOS]
+    public static let supportsTouch: Platforms = [.iOS, .watchOS, .visionOS]
+    public static let supportsKeyboard: Platforms = [.iOS, .macOS, .visionOS]
+    public static let supportsPointer: Platforms = [.iOS, .macOS, .visionOS]
+    
+    /// Whether the platforms include the current device.
+    public var includeCurrentDevice: Bool {
+        #if os(macOS)
+        self.contains(.macOS)
+        #elseif os(iOS)
+        self.contains(.iOS)
+        #elseif os(tvOS)
+        self.contains(.tvOS)
+        #elseif os(visionOS)
+        self.contains(.visionOS)
+        #elseif os(watchOS)
+        self.contains(.watchOS)
+        #else
+        false
+        #endif
+    }
+}
+
 /// A new feature of the app.
 public struct NewFeature: Identifiable, Hashable, Sendable {
     
-    public init(title: String, description: AttributedString?, iconName: String) {
+    public init(title: String, description: AttributedString?, iconName: String, supportedPlatforms: Platforms = .all) {
         self.title = title
         self.description = description
         self.iconName = iconName
+        self.supportedPlatforms = supportedPlatforms
     }
     
     /// The title of the new feature.
@@ -77,6 +115,9 @@ public struct NewFeature: Identifiable, Hashable, Sendable {
     
     /// The SF symbol corresponding to the new feature.
     public let iconName: String
+    
+    /// The platforms the feature is available on.
+    public let supportedPlatforms: Platforms
     
     public let id = UUID()
 }
