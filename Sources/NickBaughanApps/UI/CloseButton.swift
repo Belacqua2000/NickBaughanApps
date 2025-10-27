ca61a0efc87466cd2bec647fc3a61624c36aa324
Nick Baughan <nick.baughan@icloud.com>
2025-10-27 13:41:22 +0000

    Updated Close button for OS 26.

--- Sources/NickBaughanApps/UI/CloseButton.swift
+++ Sources/NickBaughanApps/UI/CloseButton.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-public struct CloseButton: ToolbarContent {
+public struct CloseButton: CustomizableToolbarContent {
     public init() { }
     
     public init(title: String? = nil, toolbarPlacement: ToolbarItemPlacement? = nil) {
@@ -21,6 +21,7 @@ public struct CloseButton: ToolbarContent {
     }
     
     @Environment(\.dismiss) private var dismiss
+    @Environment(\.isPresented) private var isPresented
     
     var title: String = String(localized: "Done", bundle: .module, comment: "The title of the close button.")
     
@@ -32,67 +33,54 @@ public struct CloseButton: ToolbarContent {
         #endif
     }()
     
-    let symbolVariant: SymbolVariants = {
-        if #available(iOS 26, *) {
-            .none
-        } else {
-            .fill.circle
-        }
-    }()
-    
-    let tint: Color? = {
-        if #available(iOS 26, *) {
-            nil
-        } else {
-            .gray
-        }
-    }()
-    
-    let font: Font? = {
-        if #available(iOS 26, *) {
-            nil
-        } else {
-            .title2
-        }
-    }()
-    
-    public var body: some ToolbarContent {
-        ToolbarItem(placement: placement) {
-            Button(action: dismiss.callAsFunction) {
-                #if os(iOS)
-                Image(systemName: "xmark")
-                    .accessibilityLabel(title)
-                #else
-                Label(title, systemImage: "xmark")
-                #endif
+    public var body: some CustomizableToolbarContent {
+        if isPresented {
+            if #available(iOS 26.0, macOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *) {
+                ToolbarItem(id: "Close") {
+                    Button(role: .close, action: dismiss.callAsFunction)
+                }
+            } else {
+                ToolbarItem(id: "Close", placement: placement) {
+                    
+                    Button(action: dismiss.callAsFunction) {
+#if os(iOS)
+                        Image(systemName: "xmark")
+                            .accessibilityLabel(title)
+#else
+                        Label(title, systemImage: "xmark")
+#endif
+                    }
+                    .help(Text("Dismiss the current view", bundle: .module, comment: "The help descriptor for the close button."))
+#if os(iOS) || os(visionOS)
+                    .keyboardShortcut(.cancelAction)
+                    .imageScale(.large)
+#endif
+                    
+#if os(iOS)
+                    .symbolVariant(.fill.circle)
+                    .font(.title2)
+                    .contentShape(.hoverEffect, .circle)
+                    .hoverEffect(.lift)
+                    .tint(.gray)
+                    .symbolRenderingMode(.hierarchical)
+#elseif os(watchOS)
+                    .labelStyle(.iconOnly)
+#elseif os(macOS)
+                    .labelStyle(.titleOnly)
+#endif
+                }
             }
-            .help(Text("Dismiss the current view", bundle: .module, comment: "The help descriptor for the close button."))
-            #if os(iOS) || os(visionOS)
-            .keyboardShortcut(.cancelAction)
-            .imageScale(.large)
-            #endif
-        
-            #if os(iOS)
-            .symbolVariant(symbolVariant)
-            .font(font)
-            .contentShape(.hoverEffect, .circle)
-            .hoverEffect(.lift)
-            .tint(tint)
-            .symbolRenderingMode(.hierarchical)
-            #elseif os(watchOS)
-            .labelStyle(.iconOnly)
-            #elseif os(macOS)
-            .labelStyle(.titleOnly)
-            #endif
         }
     }
 }
 
 #Preview {
     Color.blue
+        .ignoresSafeArea()
         .sheet(isPresented: .constant(true)) {
             NavigationStack {
                 Color.pink
+                    .ignoresSafeArea()
                     .toolbar(content: CloseButton.init)
             }
         }
